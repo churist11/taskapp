6baba1fc13f71f0103742f477f7d4383802e0cf7
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-08 20:57:56 +0900

    Recreate main VC file , and Prepare cell on storyboard

---
+++ taskapp/Controller/ListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ListViewController.swift
+//  taskapp
+//
+//  Created by 櫻井将太郎 on 2020/12/08.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+	
+
+	// MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+	}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
