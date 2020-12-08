ac899ee47c1edc016cf959a0a16f896257593e33
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-08 21:01:51 +0900

    Create file for referencing constants

---
+++ taskapp/Support/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  taskapp
+//
+//  Created by 櫻井将太郎 on 2020/12/08.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import Foundation
+
+struct C {
+	internal static let CELL_ID: String = "Cell"
+}
