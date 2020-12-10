f63aa65bbe2b47c76ae037b2c0ac81235ee31059
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-10 23:33:19 +0900

    Implement realm-update in inputVC, and reflect data from prepare method

--- taskapp/Controller/InputViewController.swift
+++ taskapp/Controller/InputViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class InputViewController: UIViewController {
 
 	// MARK: - IBOutlet
-
 
 
 	@IBOutlet weak var titleTextField: UITextField!
@@ -22,21 +22,41 @@ final class InputViewController: UIViewController {
 	// MARK: - Instance Property
 
 
-	internal var task: Task?
+	// Get instance value from Realm
+	private let realm = try! Realm()
+
+	// Guaranteed tobe assigned value
+	internal var task: Task!
 
 
 	// MARK: - LifeCycle
 
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+		// Reflect task properties into UI compornents
+		self.titleTextField.text = self.task.title
+		self.contentsTextView.text = self.task.contents
+		self.datePicker.date = self.task.date
+	}
 
 	// Deal with database about input before back to list
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
+
+		// Update realm to add new / edited task
+		try! self.realm.write {
+
+			// Configure task object
+			self.task.title = self.titleTextField.text!
+			self.task.contents = self.contentsTextView.text
+			self.task.date = self.datePicker.date
+
+			// Save task in realm
+			self.realm.add(self.task, update: .modified)
+		}
+
 	}
 
 }// MARK: - Endline
