f448debff9dd9e43aa36872c4ee0f242185d22a9
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-12 15:26:30 +0900

    Fix notification function to present notification in foreground

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-import UserNotifications 
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
 
 		}
+
+		center.delegate = self
 
 		return true
 	}
@@ -87,5 +89,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	    }
 	}
 
+}// Endline
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+		completionHandler([.alert, .sound])
+	}
 }
 
